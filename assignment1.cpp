017ed942404e42e40583149cae3ce1c3b904a59e
berra-dogan <berra.dogan129@gmail.com>
2025-04-15 19:14:27 +0200

    Fixed Examples

--- assignment1.cpp
+++ assignment1.cpp
@@ -59,14 +59,13 @@ int example1() {
     std::chrono::duration<double> duration = end_time - start_time;
     std::cout << "Elapsed time: " << duration.count() << " seconds" << std::endl;
     
-    stbi_write_png("example1.png", W, H, 3, &image[0], 0);
+    stbi_write_png("output_images/example1.png", W, H, 3, &image[0], 0);
  
     return 0;
 }
 
 int example2() {
-    #define NB_PATHS 32
-
+    int nb_paths = 32;
     int W = 512;
     int H = 512;
     Vector camera_origin(0, 0, 55);
@@ -80,11 +79,14 @@ int example2() {
         std::make_shared<Sphere>(Vector(0, 1000, 0), 940, Vector(0.2, 0.5, 0.9)),  // ceiling
         std::make_shared<Sphere>(Vector(-1000, 0, 0), 940, Vector(0.4, 0.8, 0.7)), // back-wall
         std::make_shared<Sphere>(Vector(1000, 0, 0), 940, Vector(0.9, 0.4, 0.3)),  // wall-behind-camera
-        std::make_shared<Sphere>(Vector(0, 0, 0), 10, Vector(0.8, 0.8, 0.8)), // ball in the middle
+        std::make_shared<Sphere>(Vector(-18, -2, 0), 8, Vector(0.8, 0.8, 0.8), true), // mirror
+        std::make_shared<Sphere>(Vector(18, -2, 0), 8, Vector(0.8, 0.8, 0.8), false, true, 1.5), // transparent
+        std::make_shared<Sphere>(Vector(0, -2, 0), 8, Vector(0, 0, 0), false, true, 1.5),       // hollow-transp 1
+        std::make_shared<Sphere>(Vector(0, -2, 0), 8 - EPSILON, Vector(0, 0, 0), false, true, 1), // hollow-transp 2
     };
 
     std::vector<std::shared_ptr<LightSource>> standard_scene_lights = {
-        std::make_shared<PointLight>(Vector(-10, -20, 40), 1e10),
+        std::make_shared<PointLight>(Vector(-10, 20, 40), 3e10),
     };
 
     Scene scene(objects, standard_scene_lights);
@@ -102,7 +104,7 @@ int example2() {
             // Antialiasing
             double stdev = 0.001;
             Vector pixelColor(0., 0., 0.);
-            for (int k = 0; k < NB_PATHS; k++) {
+            for (int k = 0; k < nb_paths; k++) {
                 Vector sample = random_cos(engine, r_dir);
                 sample.normalize();
                 Vector random_dir = r_dir+sample*stdev;
@@ -110,22 +112,22 @@ int example2() {
                 Ray ray(camera_origin, random_dir);
                 pixelColor = pixelColor + scene.getColor(ray, engine, false);
             }
-            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/NB_PATHS, 1/2.2));
+            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/nb_paths, 1/2.2));
         }
     }
     auto end_time = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end_time - start_time;
     std::cout << "Elapsed time: " << duration.count() << " seconds" << std::endl;
     
-    stbi_write_png("example2.png", W, H, 3, &image[0], 0);
+    stbi_write_png("output_images/example2.png", W, H, 3, &image[0], 0);
  
     return 0;
 }
 
 int example3() {
-    #define NB_PATHS 32
+    int nb_paths = 32;
 
     int W = 512;
     int H = 512;
@@ -162,7 +164,7 @@ int example3() {
             // Antialiasing
             double stdev = 0.001;
             Vector pixelColor(0., 0., 0.);
-            for (int k = 0; k < NB_PATHS; k++) {
+            for (int k = 0; k < nb_paths; k++) {
                 Vector sample = random_cos(engine, r_dir);
                 sample.normalize();
                 Vector random_dir = r_dir+sample*stdev;
@@ -170,22 +172,22 @@ int example3() {
                 Ray ray(camera_origin, random_dir);
                 pixelColor = pixelColor + scene.getColor(ray, engine, false);
             }
-            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/NB_PATHS, 1/2.2));
+            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/nb_paths, 1/2.2));
         }
     }
     auto end_time = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end_time - start_time;
     std::cout << "Elapsed time: " << duration.count() << " seconds" << std::endl;
     
-    stbi_write_png("example3.png", W, H, 3, &image[0], 0);
+    stbi_write_png("output_images/example3.png", W, H, 3, &image[0], 0);
  
     return 0;
 }
 
 int example4() {
-    #define NB_PATHS 32
+    int nb_paths = 32;
 
     int W = 512;
     int H = 512;
@@ -223,7 +225,7 @@ int example4() {
             // Antialiasing
             double stdev = 0.001;
             Vector pixelColor(0., 0., 0.);
-            for (int k = 0; k < NB_PATHS; k++) {
+            for (int k = 0; k < nb_paths; k++) {
                 Vector sample = random_cos(engine, r_dir);
                 sample.normalize();
                 Vector random_dir = r_dir+sample*stdev;
@@ -231,9 +233,9 @@ int example4() {
                 Ray ray(camera_origin, random_dir);
                 pixelColor = pixelColor + scene.getColor(ray, engine, false);
             }
-            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/NB_PATHS, 1/2.2));
+            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/nb_paths, 1/2.2));
         }
     }
     auto end_time = std::chrono::high_resolution_clock::now();
@@ -246,8 +248,7 @@ int example4() {
 }
 
 int example_mesh(){
-
-    #define NB_PATHS 32
+    int nb_paths = 5;
 
     int W = 512;
     int H = 512;
@@ -291,7 +292,7 @@ int example_mesh(){
             // Antialiasing
             double stdev = 0.001;
             Vector pixelColor(0., 0., 0.);
-            for (int k = 0; k < NB_PATHS; k++) {
+            for (int k = 0; k < nb_paths; k++) {
                 Vector sample = random_cos(engine, r_dir);
                 sample.normalize();
                 Vector random_dir = r_dir+sample*stdev;
@@ -299,21 +300,25 @@ int example_mesh(){
                 Ray ray(camera_origin, random_dir);
                 pixelColor = pixelColor + scene.getColor(ray, engine, false);
             }
-            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/NB_PATHS, 1/2.2));
-            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/NB_PATHS, 1/2.2));
+            image[(i * W + j) * 3 + 0] = std::min(255., std::pow(pixelColor[0]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 1] = std::min(255., std::pow(pixelColor[1]/nb_paths, 1/2.2));
+            image[(i * W + j) * 3 + 2] = std::min(255., std::pow(pixelColor[2]/nb_paths, 1/2.2));
         }
     }
     auto end_time = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end_time - start_time;
     std::cout << "Elapsed time: " << duration.count() << " seconds" << std::endl;
     
-    stbi_write_png("example_mesh.png", W, H, 3, &image[0], 0);
+    stbi_write_png("output_images/example_mesh.png", W, H, 3, &image[0], 0);
  
     return 0;
 }
 
 
 int main() {
-    return example4();
+    example1();
+    example2();
+    example3();
+    example4();
+    example_mesh();
 }
