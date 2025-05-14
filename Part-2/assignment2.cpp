2b552359044c59c0dec83d65ef305f839cb34424
berra-dogan <berra.dogan129@gmail.com>
2025-05-14 18:02:59 +0200

    polygon attributes

--- Part-2/assignment2.cpp
+++ Part-2/assignment2.cpp
@@ -9,8 +9,29 @@ class Polygon {
 
 public:
 
-    static Vector get_normal2D(Vector& v){
-        return Vector(v[0], -v[1], 0);
+    double area() {
+        double s = 0;
+        for (int i = 0; i < vertices.size(); i++){ 
+            int ip = (i==vertices.size()-1) ? 0 : (i+1);
+            s += std::abs(vertices[i][0]*vertices[ip][1]-vertices[ip][0]*vertices[i][1]);
+        }
+        return s/2.;
+    }
+
+    double integral_square_distance(const Vector& Pi){
+        double s = 0;
+        for (int t=0; t<vertices.size()-2; t++){
+            Vector c[3] = {vertices[0], vertices[t], vertices[t+1]};
+            double integralT = 0;
+            for (int k=0; k<3; k++) {
+                for (int l = k; l < 3; l++){
+                    integralT += dot(c[k]-Pi, c[l]-Pi);
+                }
+            }
+            double areaT = std::abs(((c[1][1]-c[0][1])*(c[2][0]-c[0][0]))-((c[1][0]-c[0][0])*(c[2][0]-c[1][1])));
+            s += integralT * areaT / 6;
+        }
+        return s;
     }
    
     std::vector<Vector> vertices;
@@ -146,134 +167,6 @@ void save_svg_animated(const std::vector<Polygon> &polygons, std::string filenam
         fprintf(f, "</svg>\n");
     }
     fclose(f);
-}
-
-#include <stdio.h>
-#include <lbfgs.h>
-
-class objective_function
-{
-protected:
-    lbfgsfloatval_t *m_x;
-
-public:
-    objective_function() : m_x(NULL)
-    {
-    }
-
-    virtual ~objective_function()
-    {
-        if (m_x != NULL) {
-            lbfgs_free(m_x);
-            m_x = NULL;
-        }
-    }
-
-    int run(int N)
-    {
-        lbfgsfloatval_t fx;
-        lbfgsfloatval_t *m_x = lbfgs_malloc(N);
-
-        if (m_x == NULL) {
-            printf("ERROR: Failed to allocate a memory block for variables.\n");
-            return 1;
-        }
-
-        /* Initialize the variables. */
-        for (int i = 0;i < N;i += 2) {
-            m_x[i] = -1.2;
-            m_x[i+1] = 1.0;
-        }
-
-        /*
-            Start the L-BFGS optimization; this will invoke the callback functions
-            evaluate() and progress() when necessary.
-         */
-        int ret = lbfgs(N, m_x, &fx, _evaluate, _progress, this, NULL);
-
-        /* Report the result. */
-        printf("L-BFGS optimization terminated with status code = %d\n", ret);
-        printf("  fx = %f, x[0] = %f, x[1] = %f\n", fx, m_x[0], m_x[1]);
-        
-        return ret;
-    }
-
-protected:
-    static lbfgsfloatval_t _evaluate(
-        void *instance,
-        const lbfgsfloatval_t *x,
-        lbfgsfloatval_t *g,
-        const int n,
-        const lbfgsfloatval_t step
-        )
-    {
-        return reinterpret_cast<objective_function*>(instance)->evaluate(x, g, n, step);
-    }
-
-    lbfgsfloatval_t evaluate(
-        const lbfgsfloatval_t *x,
-        lbfgsfloatval_t *g,
-        const int n,
-        const lbfgsfloatval_t step
-        )
-    {
-        lbfgsfloatval_t fx = 0.0;
-
-        for (int i = 0;i < n;i += 2) {
-            lbfgsfloatval_t t1 = 1.0 - x[i];
-            lbfgsfloatval_t t2 = 10.0 * (x[i+1] - x[i] * x[i]);
-            g[i+1] = 20.0 * t2;
-            g[i] = -2.0 * (x[i] * g[i+1] + t1);
-            fx += t1 * t1 + t2 * t2;
-        }
-        return fx;
-    }
-
-    static int _progress(
-        void *instance,
-        const lbfgsfloatval_t *x,
-        const lbfgsfloatval_t *g,
-        const lbfgsfloatval_t fx,
-        const lbfgsfloatval_t xnorm,
-        const lbfgsfloatval_t gnorm,
-        const lbfgsfloatval_t step,
-        int n,
-        int k,
-        int ls
-        )
-    {
-        return reinterpret_cast<objective_function*>(instance)->progress(x, g, fx, xnorm, gnorm, step, n, k, ls);
-    }
-
-    int progress(
-        const lbfgsfloatval_t *x,
-        const lbfgsfloatval_t *g,
-        const lbfgsfloatval_t fx,
-        const lbfgsfloatval_t xnorm,
-        const lbfgsfloatval_t gnorm,
-        const lbfgsfloatval_t step,
-        int n,
-        int k,
-        int ls
-        )
-    {
-        printf("Iteration %d:\n", k);
-        // printf("  fx = %f, x[0] = %f, x[1] = %f\n", fx, x[0], x[1]);
-        printf("  fx = %f", fx);
-        printf("  xnorm = %f, gnorm = %f, step = %f\n", xnorm, gnorm, step);
-        printf("\n");
-        return 0;
-    }
-};
-
-
-
-#define N   100
-
-int main(int argc, char *argv)
-{
-    objective_function obj;
-    return obj.run(N);
 }
 
 int main() {
